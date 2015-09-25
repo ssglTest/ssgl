ad571e76dc49bf69b9cd8146e731ed49f1c6c407
zhan <zhan@32OS7KT2E1S0MQG>
2015-09-25 18:13:47 +0800

    完成了学生获取自己上传的论文选题申请

--- src/com/sdjz/paperTitleApply/PaperTitleApplyController.java
+++ src/com/sdjz/paperTitleApply/PaperTitleApplyController.java
@@ -14,6 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.sdjz.domain.Actor;
 import com.sdjz.domain.PaperTitleApply;
+import com.sdjz.domain.Student;
 import com.sdjz.help.CommonHelp;
 import com.sdjz.service.PaperTitleApplyService;
 
@@ -23,6 +24,24 @@ public class PaperTitleApplyController {
 
 	@Autowired
 	private PaperTitleApplyService paperTitleApplyService;
+	
+	/**
+	 * 根据当前学生获取提交的论文选题申请表
+	 * @param httpSession
+	 * @param modelMap
+	 * @return
+	 */
+	@RequestMapping("/getPaperTitleApplyByStudent.html")
+	public String getPaperTitleApplyByStudent(HttpSession httpSession,ModelMap modelMap){
+		//获取当前学生
+		Student student = (Student)CommonHelp.getCurrentActor(httpSession);
+		//得到PaperTitleApply对象
+		PaperTitleApply paperTitleApply = student.getPaperTitleApply();
+		//添加到modelMap中，在jsp中获取
+		modelMap.put("paperTitleApply", paperTitleApply);
+		return "paperTitleApply/paperTitleApplyListByStudent";
+	}
+	
 	@RequestMapping("/paperTitleApplyManage.html")
 	/**
 	 * 列出所有的论文选题申请表
@@ -31,7 +50,6 @@ public class PaperTitleApplyController {
 	 * @return
 	 */
 	public String paperTitleApplyManager(HttpSession session,ModelMap modelMap){
-		//Actor actor = CommonHelp.getCurrentActor(session);
 		List<PaperTitleApply> paperTitleApplyList = paperTitleApplyService.findAll();
 		modelMap.put("paperTitleApplyList", paperTitleApplyList);
 		return "paperTitleApply/paperTitleApplyList";
