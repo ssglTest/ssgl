98cd9d83a4a15b5d28904563dac106ad34c94c12
yangwenfei <728427899@qq.com>
2015-11-25 16:14:45 +0800

    测试major与school的关联查询

--- src/com/sdjz/service/MajorService.java
+++ src/com/sdjz/service/MajorService.java
@@ -1,8 +1,20 @@
 package com.sdjz.service;
 
 
+import java.util.List;
+import java.util.Map;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+import org.springside.modules.persistence.DynamicSpecifications;
+import org.springside.modules.persistence.SearchFilter;
+import org.springside.modules.persistence.SearchFilter.Operator;
 
 import com.sdjz.dao.MajorDao;
 import com.sdjz.domain.Major;
@@ -20,6 +32,11 @@ public class MajorService extends BasicService<Major,Integer>{
 		this.majorDao=(MajorDao)basicDao;
 		
 	}
-	
+	public List<Major> find(Integer schoolId,Map<String,Object> searchParams){
+		Map<String, SearchFilter> filters = SearchFilter.parse(searchParams);
+      filters.put("school.id", new SearchFilter("user.id", Operator.EQ, schoolId));
+      Specification<Major> spec = DynamicSpecifications.bySearchFilter(filters.values(), Major.class);
+		return majorDao.findAll(spec);
+	}
 
 }
--- src/com/sdjz/serviceTest/MajorServiceTest.java
+++ src/com/sdjz/serviceTest/MajorServiceTest.java
@@ -33,5 +33,9 @@ public class MajorServiceTest {
 		majorService.save(major1);
 	
 	}
+	/*@Test
+	public void find(){
+		List<Major> major=majorService.find(Map<"id",>)
+	}*/
 
 }
